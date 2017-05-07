f16c8faff568bdae0e7745d7cb41617d23ff3791
Julio Cesar <juliocesar3094@hotmail.com>
2017-05-06 21:59:16 -0500

    Add navigation bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
 
-  get 'static_pages/about'
+  get 'static_pages/about', as: 'about'
 
-  get 'static_pages/band'
+  get 'static_pages/band', as: 'band'
 
-  get 'static_pages/sport'
+  get 'static_pages/sport', as: 'sport'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static_pages#home'
+  root 'static_pages#home', as: 'home'
 end
